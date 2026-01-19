a0548f0a836f9345b46ac8264c213b1e9cb1b61f
MehtabWazir34 <mehtabwazir34@gmail.com>
2026-01-20 04:13:16 +0500

    added

--- clientside/src/Pages/Products.jsx
+++ clientside/src/Pages/Products.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router";
 import {InPut} from '../Inputs/InPuts'
 
 function Products() {
-  const [category, setCat] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
+  // const [category, setCat] = useState([]);
+  // const [filteredProducts, setFilteredProducts] = useState([]);
   const [activeCategory, setActiveCategory] = useState([]);
   const [priceRange, setPriceRange] = useState({'low':'', 'high':''})
-    
   const [productsList, setProducts] = useState([]);
+
   useEffect(()=>{
    const getFilteredItems = async()=>{
     try {
@@ -32,35 +32,28 @@ function Products() {
    getFilteredItems();
   }, [activeCategory, priceRange])
 
-  /* Category Filter Logic */
-  // const handleCategory = () => {
-  //   setActiveCategory(category);
-
-  //   if (category === "All") {
-  //     setFilteredProducts(productsList);
-  //   } else {
-  //     const filtered = productsList.filter(
-  //       (item) => item.Category === category
-  //     );
-  //     setFilteredProducts(filtered);
-  //   }
-  // };
 
   return (
-    <section className="max-w-7xl mx-auto h-[calc(100vh-120px)] grid grid-cols-1 md:grid-cols-[260px_1fr] gap-6 p-6">
+    <section className="max-w-7xl mx-auto h-[calc(100vh-120px)] grid grid-cols-1 md:grid-cols-[260px_1fr] gap-6 px-4 py-6">
 
       {/* Sidebar */}
-      <aside className="bg-[#2c3639] rounded-2xl p-4 h-full">
+      <aside className="bg-[#2c3639] rounded-2xl p-2 md:p-4 h-full">
         <h4 className="text-[#ffe2af] font-semibold border-b-2 border-amber-600 pb-2 mb-4">
           Browse Categories
         </h4>
-<ul className="space-y-2 text-sm">
-  {["cricket", "Pants", "Shoes"].map((cat) => (
+<div className="m-4 space-2 gap-2 max-w-full md:w-full flex md:flex-col flex-row justify-center">
+  <input className="max-w-1/2 rounded-md md:w-full border border-gray-100 text-gray-100 p-2" type={'number'} placeholder={'Enter min price'} onChange={(a)=> setPriceRange({...priceRange, low: a.target.value})}  />
+  <input className="max-w-1/2 rounded-md md:w-full border border-gray-100 text-gray-100 p-2" type={'number'} placeholder={'Enter max price'} onChange={(a)=> setPriceRange({...priceRange, high: a.target.value})} />
+
+</div>
+<ul className="w-full space-y-2 text-lg leading-tight flex justify-center gap-2 sm:justify-around md:grid md:grid-cols-1">
+  {["Cricket", "Football", "Vollyball", "Wears"].map((cat) => (
     <li key={cat}>
       <label className="flex items-center gap-2 text-[#f2d39a]">
         <input
-          type="checkbox"
-          value={cat}
+        className=""
+          type={"checkbox"}
+          value={activeCategory}
           onChange={(e) =>
             setActiveCategory((prev) =>
               e.target.checked
@@ -74,29 +67,17 @@ function Products() {
     </li>
   ))}
 </ul>
-<div className="mt-4">
-  <InPut type={'number'} placeholder={'Enter min price'} onChange={(a)=> setPriceRange({...priceRange, low: a.target.value})}  />
-  <InPut type={'number'} placeholder={'Enter max price'} onChange={(a)=> setPriceRange({...priceRange, high: a.target.value})} />
-  {/* <input
-    type="number"
-    placeholder="Max Price"
-    className="w-full p-1 rounded"
-    onChange={(e) =>
-      setPriceRange({ ...priceRange, high: e.target.value })
-    }
-  /> */}
-</div>
 
       </aside>
 
       {/* Products Scroll Area */}
-      <section className="bg-[#2c3639] rounded-2xl p-4 overflow-y-auto">
+      <section className="bg-[#2c3639] rounded-2xl p-4 min-h-screen md:overflow-y-auto">
         {productsList.length === 0 ? (
           <p className="text-center text-gray-300 mt-10">
             No products found.
           </p>
         ) : (
-          <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6">
+          <div className="flex flex-col md:grid  md:grid-cols-3 lg:grid-cols-5 gap-6">
             {productsList.map((item) => (
               <Link
                 key={item._id}
