74b60b69497009246a79b5197c6233c36a5205cd
MehtabWazir34 <mehtabwazir34@gmail.com>
2026-03-02 19:22:52 +0500

    ...

--- MVPSrvr/ROutes/adminRoutes.js
+++
@@ -1,5 +0,0 @@
-import {Router} from 'express'
-import { adminCheck } from '../authCheck'
-const adminRoutes = Router()
-
-adminRoutes.patch('/:orderId/status', adminCheck)
--- MVPSrvr/authCheck.js
+++
@@ -1,9 +0,0 @@
-export const adminCheck = (req, res, next) => {
-  const adminKey = req.headers["x-admin-key"];
-
-  if (!adminKey || adminKey !== process.env.ADMIN_KEY) {
-    return res.status(401).json({ message: "Admin access only" });
-  }
-
-  next();
-};
