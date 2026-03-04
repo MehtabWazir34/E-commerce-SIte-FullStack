60485dd7c3a48c976ca86d077b5c6e0978f2a7ac
MehtabWazir34 <mehtabwazir34@gmail.com>
2026-03-05 02:56:22 +0500

    Final

--- serverside/api/index.js
+++ serverside/api/index.js
@@ -1,3 +1,34 @@
-import myApp from "../app.js";
+import express from 'express'
+import cors from 'cors'
+import { ConnectDb } from '../DB/ConnectDB.js';
+import { configDotenv } from 'dotenv';
+import { userRoutes } from '../Routes/userRoutes.js';
+import { productRoutes } from '../Routes/productRoutes.js';
+import path from 'path'
+import { adminRoutes } from '../Routes/AdminRoutes.js';
+// import { orderRoutes } from './Routes/orderRoutes.js';
+configDotenv()
+const myApp = express();
+myApp.use(express.json());
+myApp.use(cors({
+    frontURL : process.env.FRONTEND_urL || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], 
+    credentials: true
+}));
+
+ConnectDb();
+
+myApp.use("/user", userRoutes);
+myApp.use('/products', productRoutes);
+myApp.use('/admin', adminRoutes);
+
+myApp.use('/uploads', express.static(path.join(process.cwd(),"uploads")))
+
+// myApp.listen(3400, ()=>{
+//     console.log(
+//         `Its running! localhost:3400`
+//     );
+    
+// })
 
 export default myApp
