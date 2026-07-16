97d057f9dd461acd72ab640631992964d0089327
MehtabWazir34 <mehtabwazir34@gmail.com>
2026-07-16 22:35:43 +0500

    Render

--- serverside/ServerControllers/productControllers.js
+++ serverside/ServerControllers/productControllers.js
@@ -1,4 +1,4 @@
-import cloudinary from "../Config/cloudinary.js";
+import cloudinary from "../cnfg/cloudii.js";
 import theProduct from "../DBModels/ProductModel.js";
 
 export const addProducts = async (req, res) => {
